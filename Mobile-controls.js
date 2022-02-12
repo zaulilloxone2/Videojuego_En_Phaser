82a1be48aab5659d8e60e2eba5cc57b0b3e79d7a
Zaulilloxone2 <Saul.Garcia4654@alumnos.udg.mx>
2022-02-12 11:08:17 -0600

    Juego terminado, solo falta estillizar

---
+++ Mobile-controls.js
@@ -0,0 +1,36 @@
+export class MobileControls {
+    constructor(scene) {
+      this.relatedScene = scene;
+      this.right = {isDown: false};
+      this.left = { isDown: false };
+    }
+    
+    create() {
+      this.leftButton = this.relatedScene.add.image(60, 320, 'leftbutton').setScale(1.3).setInteractive({ draggable: true });
+      this.rightButton = this.relatedScene.add.image(740, 320, 'rightbutton').setScale(1.3).setInteractive({ draggable: true });
+  
+      this.leftButton.on('pointerdown', () => {
+        this.left.isDown = true;
+        this.right.isDown = false;
+        this.launch.isDown = false;
+      });
+      this.leftButton.on('pointerup', () => {
+        this.left.isDown = false;
+      });
+      this.leftButton.on('dragend', () => {
+        this.left.isDown = false;
+      });
+  
+      this.rightButton.on('pointerdown', () => {
+        this.right.isDown = true;
+        this.left.isDown = false;
+        this.launch.isDown = false;
+      });
+      this.rightButton.on('pointerup', () => {
+        this.right.isDown = false;
+      });
+      this.rightButton.on('dragend', () => {
+        this.right.isDown = false;
+      });
+    }
+  }
--- game.js
+++ game.js
@@ -1,15 +1,24 @@
+import { Scoreboard } from "./Scoreboard.js";
+import { MobileControls } from "./Mobile-controls.js";
+
 export class Game extends Phaser.Scene {
 
     constructor() {
         super({key:'game'});
     }
 
+    init(){
+        this.Scoreboard = new Scoreboard(this);
+        this.mobileControls = new MobileControls(this);
+    }
 
     preload() {
         this.load.image('background','images/background.png');
         this.load.image('gameover', 'images/gameover.png');
         this.load.image('platform','images/platform.png');
         this.load.image('ball','images/ball.png');
+        this.load.image('rightbutton', '/images/right.png');
+        this.load.image('leftbutton', '/images/left.png');
     }
 
     create() {
@@ -20,6 +29,8 @@ export class Game extends Phaser.Scene {
         this.gameoverImage = this.add.image(400, 90, 'gameover');
 
         this.gameoverImage.visible=false;
+
+        this.Scoreboard.create();
 
         this.platform=this.physics.add.image(400,460,'platform').setImmovable();
 
@@ -33,44 +44,34 @@ export class Game extends Phaser.Scene {
             velocity = 0 - velocity;
         }
         this.ball.setVelocity(velocity,10);
-        this.physics.add.collider(this.ball,this.platform);
+        this.physics.add.collider(this.ball,this.platform,this.platformImpact,null,this);
 
         this.ball.setBounce(1);
+        this.mobileControls.create();
 
         this.cursors = this.input.keyboard.createCursorKeys();
+    }
 
+
+    platformImpact(){
+        this.Scoreboard.incrementPoints(1);
     }
 
     update(){
-
-        if(this.cursors.left.isDown){
-            this.platform.setVelocityX(-500);
+        
+        if(this.cursors.left.isDown||this.mobileControls.left.isDown){
+            this.platform.setVelocityX(-700);
         }
-        else if(this.cursors.right.isDown){
-            this.platform.setVelocityX(500);
+        else if(this.cursors.right.isDown||this.mobileControls.right.isDown){
+            this.platform.setVelocityX(700);
         }
         else{
             this.platform.setVelocityX(0);
         }
 
         if (this.ball.y > 500){
-            console.log('fin');
             this.gameoverImage.visible=true;
             this.scene.pause();
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 } 
